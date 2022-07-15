96e4223a6d293eb10c49dc6690b3de9a3b2e3534
Ramkumar <87489042+Ramkumar-Programer@users.noreply.github.com>
2022-07-15 15:36:20 +0530

    Update listOfSoftware.cpp

--- listOfSoftware.cpp
+++ listOfSoftware.cpp
@@ -21,17 +21,17 @@ int main(int argc, char **argv)
     }
 
     // Initialize
-    //hres =  CoInitializeSecurity(
-    //        NULL,
-    //        -1,      // COM negotiates service
-    //        NULL,    // Authentication services
-    //        NULL,    // Reserved
-    //        RPC_C_AUTHN_LEVEL_DEFAULT,    // authentication
-    //        RPC_C_IMP_LEVEL_IMPERSONATE,  // Impersonation
-    //        NULL,             // Authentication info
-    //        EOAC_NONE,        // Additional capabilities
-    //        NULL              // eserved
-    //);
+    hres =  CoInitializeSecurity(
+           NULL,
+           -1,      // COM negotiates service
+           NULL,    // Authentication services
+           NULL,    // Reserved
+           RPC_C_AUTHN_LEVEL_DEFAULT,    // authentication
+           RPC_C_IMP_LEVEL_IMPERSONATE,  // Impersonation
+           NULL,             // Authentication info
+           EOAC_NONE,        // Additional capabilities
+           NULL              // eserved
+    );
 
 
     if (FAILED(hres))
@@ -68,17 +68,17 @@ int main(int argc, char **argv)
     // current user and obtain pointer pSvc
     // to make IWbemServices calls.
 
-    //hres = pLoc->ConnectServer(
+    hres = pLoc->ConnectServer(
 
-    //        _bstr_t(L"ROOT\\CIMV2"), // WMI namespace
-    //        NULL,                    // User name
-    //        NULL,                    // User password
-    //        0,                       // Locale
-    //        0,                    // Security flags
-    //        0,                       // Authority
-    //        0,                       // Context object
-    //        &pSvc                    // IWbemServices proxy
-    //);
+           _bstr_t(L"ROOT\\CIMV2"), // WMI namespace
+           NULL,                    // User name
+           NULL,                    // User password
+           0,                       // Locale
+           0,                    // Security flags
+           0,                       // Authority
+           0,                       // Context object
+           &pSvc                    // IWbemServices proxy
+    );
 
     if (FAILED(hres))
     {
@@ -91,89 +91,89 @@ int main(int argc, char **argv)
 
     cout << "Connected to ROOT\\CIMV2 WMI namespace" << endl;
 
-//    // Set the IWbemServices proxy so that impersonation
-//    // of the user (client) occurs.
-//    hres = CoSetProxyBlanket(
-//
-//            pSvc,                         // the proxy to set
-//            RPC_C_AUTHN_WINNT,            // authentication service
-//            RPC_C_AUTHZ_NONE,             // authorization service
-//            NULL,                         // Server principal name
-//            RPC_C_AUTHN_LEVEL_CALL,       // authentication level
-//            RPC_C_IMP_LEVEL_IMPERSONATE,  // impersonation level
-//            NULL,                         // client identity
-//            EOAC_NONE                     // proxy capabilities
-//    );
-//
-//    if (FAILED(hres))
-//    {
-//        cout << "Could not set proxy blanket. Error code = 0x"
-//             << hex << hres << endl;
-//        pSvc->Release();
-//        pLoc->Release();
-//        CoUninitialize();
-//        return 1;               // Program has failed.
-//    }
-//
-//
-//    // Use the IWbemServices pointer to make requests of WMI.
-//    // Make requests here:
-//
-//    // For example, query for all the running processes
-//    IEnumWbemClassObject* pEnumerator = NULL;
-//    hres = pSvc->ExecQuery(
-//            bstr_t("WQL"),
-//            bstr_t("SELECT * FROM Win32_Process"),
-//            WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
-//            NULL,
-//            &pEnumerator);
-//
-//    if (FAILED(hres))
-//    {
-//        cout << "Query for processes failed. "
-//             << "Error code = 0x"
-//             << hex << hres << endl;
-//        pSvc->Release();
-//        pLoc->Release();
-//        CoUninitialize();
-//        return 1;               // Program has failed.
-//    }
-//    else
-//    {
-//        IWbemClassObject *pclsObj;
-//        ULONG uReturn = 0;
-//
-//        while (pEnumerator)
-//        {
-//            hres = pEnumerator->Next(WBEM_INFINITE, 1,
-//                                     &pclsObj, &uReturn);
-//
-//            if(0 == uReturn)
-//            {
-//                break;
-//            }
-//
-//            VARIANT vtProp;
-//
-//            // Get the value of the Name property
-//            hres = pclsObj->Get(L"Name", 0, &vtProp, 0, 0);
-//            wcout << "Process Name : " << vtProp.bstrVal << endl;
-//            VariantClear(&vtProp);
-//
-//            pclsObj->Release();
-//            pclsObj = NULL;
-//        }
-//
-//    }
-//
-//    // Cleanup
-//    // ========
+   // Set the IWbemServices proxy so that impersonation
+   // of the user (client) occurs.
+   hres = CoSetProxyBlanket(
 
-    /*pSvc->Release();
-    pLoc->Release();*/
-    //pEnumerator->Release();
+           pSvc,                         // the proxy to set
+           RPC_C_AUTHN_WINNT,            // authentication service
+           RPC_C_AUTHZ_NONE,             // authorization service
+           NULL,                         // Server principal name
+           RPC_C_AUTHN_LEVEL_CALL,       // authentication level
+           RPC_C_IMP_LEVEL_IMPERSONATE,  // impersonation level
+           NULL,                         // client identity
+           EOAC_NONE                     // proxy capabilities
+   );
 
-    /*CoUninitialize();*/
+   if (FAILED(hres))
+   {
+       cout << "Could not set proxy blanket. Error code = 0x"
+            << hex << hres << endl;
+       pSvc->Release();
+       pLoc->Release();
+       CoUninitialize();
+       return 1;               // Program has failed.
+   }
+
+
+   // Use the IWbemServices pointer to make requests of WMI.
+   // Make requests here:
+
+   // For example, query for all the running processes
+   IEnumWbemClassObject* pEnumerator = NULL;
+   hres = pSvc->ExecQuery(
+           bstr_t("WQL"),
+           bstr_t("SELECT * FROM Win32_Process"),
+           WBEM_FLAG_FORWARD_ONLY | WBEM_FLAG_RETURN_IMMEDIATELY,
+           NULL,
+           &pEnumerator);
+
+   if (FAILED(hres))
+   {
+       cout << "Query for processes failed. "
+            << "Error code = 0x"
+            << hex << hres << endl;
+       pSvc->Release();
+       pLoc->Release();
+       CoUninitialize();
+       return 1;               // Program has failed.
+   }
+   else
+   {
+       IWbemClassObject *pclsObj;
+       ULONG uReturn = 0;
+
+       while (pEnumerator)
+       {
+           hres = pEnumerator->Next(WBEM_INFINITE, 1,
+                                    &pclsObj, &uReturn);
+
+           if(0 == uReturn)
+           {
+               break;
+           }
+
+           VARIANT vtProp;
+
+           // Get the value of the Name property
+           hres = pclsObj->Get(L"Name", 0, &vtProp, 0, 0);
+           wcout << "Process Name : " << vtProp.bstrVal << endl;
+           VariantClear(&vtProp);
+
+           pclsObj->Release();
+           pclsObj = NULL;
+       }
+
+   }
+
+   // Cleanup
+   // ========
+
+    pSvc->Release();
+    pLoc->Release();
+    pEnumerator->Release();
+
+    CoUninitialize();
 
     return 0;   // Program successfully completed.
 }
